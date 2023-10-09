313d554f16f8e240031e2d47e5db21e872ca3924
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-10-09 22:27:39 +0200

    Adding first confetin and notification template

---
+++ force-app/main/default/lwc/gameForceNotification/gameForceNotification.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+//Custom Labels
+import KeepItUpLabel from '@salesforce/label/c.KeepItUp';
+import NoAchievementsUnlockedYetLabel from '@salesforce/label/c.NoAchievementsUnlockedYet';
+
+export default class GameForceNotification extends LightningElement {
+    labels = {
+        KeepItUpLabel, NoAchievementsUnlockedYetLabel
+    };
+
+    isLoading
+    achievementEventHandled = true
+    closesAchievement
+
+}

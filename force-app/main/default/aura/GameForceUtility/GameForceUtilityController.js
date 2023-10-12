a247a7161cfb6c64ce08636139c691874e41ff08
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-10-12 13:34:41 +0200

    Adding Aura wrapper for Utility component Handling achievement reached event

---
+++ force-app/main/default/aura/GameForceUtility/GameForceUtilityController.js
@@ -0,0 +1,70 @@
+({
+    // Sets an empApi error handler on component initialization
+    onInit : function(component, event, helper) {
+        const empApi = component.find('empApi');
+
+        const channel = '/event/AchievementReached__e';
+        // Replay option to get new events
+        const replayId = -1;
+
+        // Subscribe to an event
+        empApi.subscribe(channel, replayId, $A.getCallback(eventReceived => {
+            // Process event (this is called each time we receive an event)
+            console.log('Received event ', JSON.stringify(eventReceived));
+            var utilityAPI = component.find("utilitybar");
+            utilityAPI.openUtility();
+        }))
+        .then(subscription => {
+            // Subscription response received.
+            // We haven't received an event yet.
+            console.log('Subscription request sent to: ', subscription.channel);
+            // Save subscription to unsubscribe later
+            component.set('v.subscription', subscription);
+
+        });
+
+
+        empApi.onError($A.getCallback(error => {
+            // Error can be any type of error (subscribe, unsubscribe...)
+            console.error('EMP API error: ', JSON.stringify(error));
+        }));
+    },
+
+    // Invokes the subscribe method on the empApi component
+    subscribe : function(component, event, helper) {
+        // Get the empApi component
+        const empApi = component.find('empApi');
+        // Get the channel from the input box
+        const channel = component.find('channel').get('v.value');
+        // Replay option to get new events
+        const replayId = -1;
+
+        // Subscribe to an event
+        empApi.subscribe(channel, replayId, $A.getCallback(eventReceived => {
+            // Process event (this is called each time we receive an event)
+            console.log('Received event ', JSON.stringify(eventReceived));
+        }))
+        .then(subscription => {
+            // Subscription response received.
+            // We haven't received an event yet.
+            console.log('Subscription request sent to: ', subscription.channel);
+            // Save subscription to unsubscribe later
+            component.set('v.subscription', subscription);
+        });
+    },
+
+    // Invokes the unsubscribe method on the empApi component
+    unsubscribe : function(component, event, helper) {
+        // Get the empApi component
+        const empApi = component.find('empApi');
+        // Get the subscription that we saved when subscribing
+        const subscription = component.get('v.subscription');
+
+        // Unsubscribe from event
+        empApi.unsubscribe(subscription, $A.getCallback(unsubscribed => {
+          // Confirm that we have unsubscribed from the event channel
+          console.log('Unsubscribed from channel '+ unsubscribed.subscription);
+          component.set('v.subscription', null);
+        }));
+    }
+})

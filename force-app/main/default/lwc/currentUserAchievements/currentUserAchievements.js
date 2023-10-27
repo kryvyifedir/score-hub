79ff5cebcf07328035ba85ee3c87dff00c7c66d3
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-10-27 14:16:02 +0200

    Empty state for Your Achievements list

--- force-app/main/default/lwc/currentUserAchievements/currentUserAchievements.js
+++ force-app/main/default/lwc/currentUserAchievements/currentUserAchievements.js
@@ -3,13 +3,22 @@ import { LightningElement, wire } from 'lwc';
 import getCurrentUserAchievementsList from '@salesforce/apex/AchievementsListController.getCurrentUserAchievementsList';
 //Custom Labels
 import YourAchievementsLabel from '@salesforce/label/c.YourAchievements';
+import ErrorLabel from '@salesforce/label/c.Error';
+import ErrorLoadingAchievementsLabel from '@salesforce/label/c.ErrorLoadingAchievements';
+import NoAchievementsFoundLabel from '@salesforce/label/c.NoAchievementsFound';
+import ContactAdministratorLabel from '@salesforce/label/c.ContactAdministrator';
 
 export default class CurrentUserAchievements extends LightningElement {
     labels = {
-        YourAchievementsLabel
+        YourAchievementsLabel,
+        ErrorLabel,
+        ErrorLoadingAchievementsLabel,
+        NoAchievementsFoundLabel,
+        ContactAdministratorLabel
     };
 
     achievements
+    showError
 
     // Getting current user info
     @wire(getCurrentUserAchievementsList)
@@ -19,18 +28,24 @@ export default class CurrentUserAchievements extends LightningElement {
                 this.achievements = data.Success
             } else if (data.Error) {
                 console.log(JSON.stringify(data.Error))
+                this.showError = true
             }
         } else if (error) {
             console.log(JSON.stringify(error))
+            this.showError = true
         }
     }
 
-    get threeMostRecentAchievements() {
+    get achievementsByDate() {
         let returnValue = [];
         if (this.achievements) {
-            returnValue = [...this.achievements].sort((a, b) => { return b.reachedDate - a.reachedDate });//.slice(0, 4);
+            returnValue = [...this.achievements].sort((a, b) => { return b.reachedDate - a.reachedDate });
         }
 
         return returnValue;
+    }
+
+    get showAchievements() {
+        return this.achievements && this.achievements.length > 0
     }
 }

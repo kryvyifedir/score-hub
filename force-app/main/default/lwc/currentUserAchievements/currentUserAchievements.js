456141445e8caf475d9b2d33ac4a1adf036b95dd
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-10-27 19:23:11 +0200

    Issue 64 - improving empty screens (#80)
    * Empty state for Your Achievements list
    
    * Improvements to empty screens and loading screens

--- force-app/main/default/lwc/currentUserAchievements/currentUserAchievements.js
+++ force-app/main/default/lwc/currentUserAchievements/currentUserAchievements.js
@@ -3,13 +3,23 @@ import { LightningElement, wire } from 'lwc';
 import getCurrentUserAchievementsList from '@salesforce/apex/AchievementsListController.getCurrentUserAchievementsList';
 //Custom Labels
 import YourAchievementsLabel from '@salesforce/label/c.YourAchievements';
+import ErrorLabel from '@salesforce/label/c.Error';
+import ErrorLoadingAchievementsLabel from '@salesforce/label/c.ErrorLoadingAchievements';
+import NoAchievementsFoundLabel from '@salesforce/label/c.NoAchievementsFound';
+import ContactAdministratorLabel from '@salesforce/label/c.ContactAdministrator';
 
 export default class CurrentUserAchievements extends LightningElement {
     labels = {
-        YourAchievementsLabel
+        YourAchievementsLabel,
+        ErrorLabel,
+        ErrorLoadingAchievementsLabel,
+        NoAchievementsFoundLabel,
+        ContactAdministratorLabel
     };
 
     achievements
+    isError = false
+    isLoading = true
 
     // Getting current user info
     @wire(getCurrentUserAchievementsList)
@@ -17,20 +27,33 @@ export default class CurrentUserAchievements extends LightningElement {
         if (data) {
             if (data.Success) {
                 this.achievements = data.Success
+                this.isLoading = false;
             } else if (data.Error) {
                 console.log(JSON.stringify(data.Error))
+                this.isError = true
+                this.isLoading = false;
             }
         } else if (error) {
             console.log(JSON.stringify(error))
+            this.isError = true
+            this.isLoading = false;
         }
     }
 
-    get threeMostRecentAchievements() {
+    get achievementsByDate() {
         let returnValue = [];
         if (this.achievements) {
-            returnValue = [...this.achievements].sort((a, b) => { return b.reachedDate - a.reachedDate });//.slice(0, 4);
+            returnValue = [...this.achievements].sort((a, b) => { return b.reachedDate - a.reachedDate });
         }
 
         return returnValue;
+    }
+
+    get showAchievements() {
+        return this.achievements && this.achievements.length > 0
+    }
+
+    get showEmpty() {
+        return !this.isLoading && !(this.achievements && this.achievements.length > 0)
     }
 }

53482e04acbfbd09d2d1602837cbce01f8f4a0f5
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-10-31 21:54:37 +0100

    Adding pretty UI for closest achievement and reached

--- force-app/main/default/lwc/achievementListItem/achievementListItem.js
+++ force-app/main/default/lwc/achievementListItem/achievementListItem.js
@@ -1,11 +1,5 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AchievementListItem extends LightningElement {
-    @api title
-    @api description
-    @api isReached
-
-    get getAchievementFallbackIcon() {
-        return this.isReached ? "standard:task2" : "standard:trailhead"
-    }
+    @api achievement
 }

3f474b3cf6b905db9de23ce57a7f8d8a520a97a3
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-12-19 22:45:30 +0100

    Adding labels and small changes to layout

--- force-app/main/default/lwc/compareUsersModal/compareUsersModal.js
+++ force-app/main/default/lwc/compareUsersModal/compareUsersModal.js
@@ -5,7 +5,20 @@ import LightningModal from 'lightning/modal';
 import getUserInfoById from '@salesforce/apex/UserInfoController.getUserInfoById';
 import getUserAchievementsById from '@salesforce/apex/AchievementsInfoController.getUserAchievementsById';
 
+//Custom Labels
+import CompareAchievementsHeaderLabel from '@salesforce/label/c.CompareAchievementsHeader';
+import CloseButtonLabel from '@salesforce/label/c.Close'
+import ErrorLabel from '@salesforce/label/c.Error';
+import ErrorLoadingAchievementsLabel from '@salesforce/label/c.ErrorLoadingAchievements';
+
 export default class CompareUsersModal extends LightningModal {
+    labels = {
+        CompareAchievementsHeaderLabel,
+        CloseButtonLabel,
+        ErrorLabel,
+        ErrorLoadingAchievementsLabel
+    };
+    
     @api content;
     userInfo1
     achievementsList1;
@@ -13,6 +26,7 @@ export default class CompareUsersModal extends LightningModal {
     achievementsList2;
 
     isLoadingAchievements = true;
+    isLoadingAchievementsIssue = false;
 
     // Getting current user info
     @wire(getUserInfoById, { userIds: "$content" })
@@ -23,9 +37,11 @@ export default class CompareUsersModal extends LightningModal {
                 this.userInfo2 = data.Success[this.content[1]]
             } else if (data.Error) {
                 console.log(JSON.stringify(data.Error))
+                this.isLoadingAchievementsIssue = true;
             }
         } else if (error) {
             console.log(JSON.stringify(error))
+            this.isLoadingAchievementsIssue = true;
         }
     }
 
@@ -41,14 +57,20 @@ export default class CompareUsersModal extends LightningModal {
             } else if (data.Error) {
                 console.log(JSON.stringify(data.Error))
                 this.isLoadingAchievements = false;
+                this.isLoadingAchievementsIssue = true;
             }
         } else if (error) {
             console.log(JSON.stringify(error))
             this.isLoadingAchievements = false;
+            this.isLoadingAchievementsIssue = true;
         }
     }
 
     handleOkay() {
         this.close('okay');
+    }
+
+    get showHeader() {
+        return !this.isLoadingAchievements && !this.isLoadingAchievementsIssue
     }
 }

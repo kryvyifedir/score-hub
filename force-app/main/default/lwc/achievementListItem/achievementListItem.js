a98f06aab36f82606a667ccd5be94c0340a49157
Fedir Kryvyi <fedirkryvyi@outlook.com>
2024-03-02 12:58:14 +0100

    Adding score number to achievements card

--- force-app/main/default/lwc/achievementListItem/achievementListItem.js
+++ force-app/main/default/lwc/achievementListItem/achievementListItem.js
@@ -3,6 +3,10 @@ import { LightningElement, api } from 'lwc';
 export default class AchievementListItem extends LightningElement {
     @api achievement
 
+    get badgeScore() {
+        return "+ " + this.achievement.score
+    }
+
     get badgeStyle() {
         return this.achievement.isReached ? "slds-theme_success" : ""
     }

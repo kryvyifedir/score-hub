357d26c00e33f709e681862472d331dcd2561301
Fedir Kryvyi <fedirkryvyi@outlook.com>
2024-03-02 12:40:00 +0100

    Adding achievements score to achievementListItem

--- force-app/main/default/lwc/achievementListItem/achievementListItem.js
+++ force-app/main/default/lwc/achievementListItem/achievementListItem.js
@@ -2,4 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 export default class AchievementListItem extends LightningElement {
     @api achievement
+
+    get badgeStyle() {
+        return this.achievement.isReached ? "slds-theme_success" : ""
+    }
 }

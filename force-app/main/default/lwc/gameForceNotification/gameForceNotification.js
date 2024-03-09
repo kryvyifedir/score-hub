2446f92844be2259c06de4533df3f05a43763e06
Fedir Kryvyi <fedirkryvyi@outlook.com>
2024-03-09 22:49:44 +0100

    Improving the Controller for NotificationCard

--- force-app/main/default/lwc/gameForceNotification/gameForceNotification.js
+++ force-app/main/default/lwc/gameForceNotification/gameForceNotification.js
@@ -15,21 +15,21 @@ export default class GameForceNotification extends LightningElement {
     @api achievementId
     @api
     get userId() {
-        return this.user_id ? this.user_id : Id;
+        return this.currentUserId ? this.currentUserId : Id;
     }
 
     set userId(value) {
-        this.user_id = value;
+        this.currentUserId = value;
     }
 
-    user_id;
+    currentUserId;
     reachedAchievement;
     closestAchievement;
 
     isLoading = false;
     isError;
 
-    @wire(getAchievementById, { achievementId: "$achievementId", userId: "$userId" })
+    @wire(getAchievementById, { achievementId: "$achievementId" })
     wiredAchievementData({ error, data }) {
         if (data) {
             if (data.Success) {
@@ -64,10 +64,10 @@ export default class GameForceNotification extends LightningElement {
     }
 
     get showReached() {
-        return this.reachedAchievement
+        return this.achievementId
     }
 
     get showClosest() {
-        return this.closestAchievement && !this.achievementId && !this.reachedAchievement
+        return !this.achievementId
     }
 }

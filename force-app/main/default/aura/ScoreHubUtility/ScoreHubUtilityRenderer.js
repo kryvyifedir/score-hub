1efa0b9a81640b4bd04d94e724b942fbdef48ed1
Fedir Kryvyi <fedirkryvyi@outlook.com>
2024-10-15 21:12:50 +0200

    Unsubscribing from platform event using empApi on unrender (#219)

--- force-app/main/default/aura/ScoreHubUtility/ScoreHubUtilityRenderer.js
+++ force-app/main/default/aura/ScoreHubUtility/ScoreHubUtilityRenderer.js
@@ -1,5 +1,12 @@
 ({
-
-// Your renderer method overrides go here
-
+    unrender : function(component, helper) {
+        this.superUnrender();
+        // Clean up event subscription
+        var empApi = component.find("empApi");
+        const subscription = component.get('v.subscription');
+        empApi.unsubscribe(subscription, $A.getCallback(unsubscribed => {
+            console.log('Unsubscribed from channel '+ unsubscribed.subscription);
+            component.set('v.subscription', null);
+        }));
+    }
 })

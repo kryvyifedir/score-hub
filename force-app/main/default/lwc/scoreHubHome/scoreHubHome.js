30e46b0667ed89395926a836969b759c9b9f3254
Fedir Kryvyi <fedirkryvyi@outlook.com>
2025-05-15 18:41:16 +0200

    Adding seasons leaderboard card header to home

--- force-app/main/default/lwc/scoreHubHome/scoreHubHome.js
+++ force-app/main/default/lwc/scoreHubHome/scoreHubHome.js
@@ -1,9 +1,10 @@
 import { LightningElement } from 'lwc';
 //Custom Labels
 import TopUsersLabel from '@salesforce/label/c.TopUsers';
+import SeasonsLeaderboardLabel from '@salesforce/label/c.SeasonsLeaderboard';
 
 export default class ScoreHubHome extends LightningElement {
     labels = {
-        TopUsersLabel
+        TopUsersLabel, SeasonsLeaderboardLabel
     };
 }

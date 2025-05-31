a45c170f3e00f8fd172162703cc1b829f0ea1534
Fedir Kryvyi <fedirkryvyi@outlook.com>
2025-05-31 14:45:27 +0200

    Fixing the issue with Standings LWC component when data for the component is missing

--- force-app/main/default/lwc/seasonStandings/seasonStandings.js
+++ force-app/main/default/lwc/seasonStandings/seasonStandings.js
@@ -1,30 +1,30 @@
 import { LightningElement, api } from 'lwc';
 
 export default class SeasonStandings extends LightningElement {
-    @api standing;
+    @api standings;
 
     get goldAvatar() {
-        return this.getAvatar(this.standing[0])
+        return this.getAvatar(this.standings?.[0])
     }
 
     get silverAvatar() {
-        return this.getAvatar(this.standing[1])
+        return this.getAvatar(this.standings?.[1])
     }
 
     get bronzeAvatar() {
-        return this.getAvatar(this.standing[2])
+        return this.getAvatar(this.standings?.[2])
     }
 
     get goldName() {
-        return this.getName(this.standing[0])
+        return this.getName(this.standings?.[0])
     }
 
     get silverName() {
-        return this.getName(this.standing[1])
+        return this.getName(this.standings?.[1])
     }
 
     get bronzeName() {
-        return this.getName(this.standing[2])
+        return this.getName(this.standings?.[2])
     }
 
     getAvatar(standing) {
